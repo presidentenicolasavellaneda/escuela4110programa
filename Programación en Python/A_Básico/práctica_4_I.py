a10befa5da8b4ee06e03926ed8cf18ee18a2dbed
JorgeAlfredoMastrandrea <81812907+JorgeAlfredoMastrandrea@users.noreply.github.com>
2024-04-11 17:12:55 -0300

    -

---
+++ Programación en Python/A_Básico/práctica_4_I.py
@@ -0,0 +1,63 @@
+# tipos de datos
+
+# tipo string o tipo cadena
+# por ejemplo para un domicilio
+calle = 'Malvinas Argentinas'
+departamento = 'San Martín'
+localidad = 'Palmira'
+
+# otro ejemplo, la patente de un auto
+patenteAutoVieja = 'AXZ 794'
+# la patente de otro auto
+patenteAutoNueva = 'AB 123 CD'
+
+# tipos número entero
+número = 1378
+dni = 40877643
+característica = 261
+teléfonoFijo = 4452637
+
+# una lista de jugadores de fútbol
+miEquipoDeFútbolIdeal = [
+    'Lev Yasin',
+    'Cafú',
+    'Paolo Maldini',
+    'Franz Beckenbauer',
+    'Roberto Carlos',
+    'Zinedine Zidane',
+    'Xavi',
+    'Maradona',
+    'Cristiano Ronaldo',
+    'Pelé',
+    'Messi'
+]
+
+# una lista de cosas para comprar en el super..
+# ojo no se le dice lista, este tipo de datos se llama
+# diccionario de datos....
+lasComprasDelSuper = {
+    'pan' : 2,
+    'chocolates' : 10,
+    'caramelos' : 40,
+    'azucar' : 2,
+    'harina' : 5,
+    'dulce de leche' : 3,
+    'galletas' : 8,
+    'jabón' : 4,
+    'asado' : 10 
+}
+
+# la temperatura ambiente
+temperaturaSanMartín = 9.3
+temperaturaJunín = 8.7
+
+# la longitud de una calle expresada en metros
+longitudCalle = 20.5
+
+# la relación entre la longitud de una circunferencia y su diámetro 
+PI = 3.141592
+
+
+# para expresar si algo es verdadero o falso
+variable1 = True
+variable2 = False
